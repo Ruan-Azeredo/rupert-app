f639cb03c10c42cce5791311a2f7d694fa950e44
Ruan-M2 <ruanazeredo@gmail.com>
2025-05-26 11:22:39 -0300

    :construction: build: use cli to install and setup automatically gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { AuthProvider } from "@/contexts/auth";
+import "@/global.css";
 import { Slot } from "expo-router";
 import React from "react";
 
 export default function RootLayout() {
 	return (
-		<AuthProvider>
-			<Slot />
-		</AuthProvider>
-	);
+        <GluestackUIProvider mode="light">
+			<AuthProvider>
+                <Slot />
+            </AuthProvider>
+		</GluestackUIProvider>
+    );
 }
