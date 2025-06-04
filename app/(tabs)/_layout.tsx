278384c0e2c9feeb75fecbec6e2e8d0cb3f47b72
Ruan-M2 <ruanazeredo@gmail.com>
2025-06-03 22:10:01 -0300

    :lipstick: style: tab Bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,47 @@
+
 import { useAuth } from "@/contexts/auth";
 import { Redirect, Tabs } from "expo-router";
+import { Bookmark, ChefHat, LogOut } from "lucide-react-native";
+import LogoutButton from "./Logout";
+
 
 export default function TabsLayout() {
-	const { user } = useAuth();
+    const { user } = useAuth();
 
 	if (!user) {
-		return <Redirect href="/(auth)" />;
+        return <Redirect href="/(auth)" />;
 	}
 
 	return (
 
         <Tabs>
             <Tabs.Screen
-                name="index"
+                name="receitas"
+                options={{
+                    title: "Receitas",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <ChefHat color={color} size={size} />
+                    ),
+                }}
             />
             <Tabs.Screen
-                name="explore"
+                name="favoritas"
+                options={{
+                    title: "Favoritas",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <Bookmark color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="Logout"
+                options={{
+                    title: "Sair",
+                    tabBarIcon: ({ focused, color, size }) => (
+                        <LogOut color={color} size={size} />
+                    ),
+                    tabBarButton: () => <LogoutButton />,
+                }}
             />
         </Tabs>
     );
